cbe08609ad94f76ace8815a3e32ad33b71a6b639
alexsa-8 <alexsa-8@mail.ru>
2024-02-28 19:36:57 +0300

    Работа над HelloController. Добавка картинок фона и персонажа. Изменение в HelloApplication.

--- src/main/java/com/example/gametwod/HelloApplication.java
+++ src/main/java/com/example/gametwod/HelloApplication.java
@@ -3,15 +3,16 @@ package com.example.gametwod;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
-public class Application extends javafx.application.Application {
+public class HelloApplication extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 715, 400);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
